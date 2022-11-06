6233ecda4a1d6a9752fbbadb970db34fbacdd602
Craig Abbott <admin@craigabbott.co.uk>
2022-11-06 12:11:31 +0000

    Styling changes

--- app.js
+++ app.js
@@ -48,9 +48,11 @@ if (process.env.NODE_ENV !== 'production') {
 }
 
 // Middlewares
+const events = require('./lib/data/events')
 app.use((req, res, next) => {
   if (req.session.data === undefined) req.session.data = {}
   res.locals.body = req.body
+  res.locals.events = events
   res.locals.data = req.session.data
   res.locals.env = process.env.NODE_ENV ? process.env.NODE_ENV : 'dev'
   next()
