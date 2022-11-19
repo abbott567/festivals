b2894d26daee2b5690ccba592870f66b98f8f960
Craig Abbott <admin@craigabbott.co.uk>
2022-11-19 20:22:55 +0000

    Updated press page

--- src/app/data/news-articles.mjs
+++ src/app/data/news-articles.mjs
@@ -1,34 +1,50 @@
 export default [
   {
     id: '1',
-    publisher: 'Shields Gazette',
+    publisher: {
+      name: 'Shields Gazette',
+      logo: '/images/logo-shields-gazette.png'
+    },
     desc: 'Great North Feast - South Shields',
     name: "12 pictures from the final day of the Great North Feast in the Park 2022 in South Shields' Bents Park",
     excerpt: 'Tempting food stalls again brought out the crowds to Bents Park on the final day of the Great North Feast in the Park.',
-    url: 'https://www.shieldsgazette.com/whats-on/things-to-do/12-pictures-from-the-final-day-of-the-great-north-feast-in-the-park-2022-in-south-shields-bents-park-3823100'
+    url: 'https://www.shieldsgazette.com/whats-on/things-to-do/12-pictures-from-the-final-day-of-the-great-north-feast-in-the-park-2022-in-south-shields-bents-park-3823100',
+    img: '/images/press-1.jpg'
   },
   {
     id: '2',
-    publisher: 'Shields Gazette',
+    publisher: {
+      name: 'Shields Gazette',
+      logo: '/images/logo-shields-gazette.png'
+    },
     desc: 'Great North Feast - South Shields',
     name: 'Great North Feast in the Park food festival returns to Bents Park this bank holiday weekend',
     excerpt: "The free Great North Feast in the Park annual food festival returns to South Tyneside with the region's finest street food traders and up and coming musicians.",
-    url: 'https://www.shieldsgazette.com/news/people/great-north-feast-in-the-park-food-festival-returns-to-bents-park-this-bank-holiday-weekend-3821034'
+    url: 'https://www.shieldsgazette.com/news/people/great-north-feast-in-the-park-food-festival-returns-to-bents-park-this-bank-holiday-weekend-3821034',
+    img: '/images/press-2.jpg'
   },
   {
     id: '3',
-    publisher: 'Chronicle',
+    publisher: {
+      name: 'Chronicle',
+      logo: '/images/logo-chronicle.png'
+    },
     desc: 'Feast by the Sea, Whitley Bay',
     name: 'Feast by the Sea returns to Whitley Bay with local food vendors serving up a treat',
-    excerpt: '',
-    url: 'https://www.chroniclelive.co.uk/whats-on/whats-on-news/gallery/whitley-bay-food-festival-returns-24810206'
+    excerpt: "Fat Hippo, Hatch 76, Simply Cheesecake and The Brownie Bar are just some of the popular North East food vendors at this weekend's Feast by the Sea",
+    url: 'https://www.chroniclelive.co.uk/whats-on/whats-on-news/gallery/whitley-bay-food-festival-returns-24810206',
+    img: '/images/press-3.webp'
   },
   {
     id: '4',
-    publisher: 'Chronicle',
+    publisher: {
+      name: 'Chronicle',
+      logo: '/images/logo-chronicle.png'
+    },
     desc: 'Generic, mentions Cramlington',
     name: 'This first Proper Food and Drink Festival of the year takes place this weekend',
-    excerpt: "Fat Hippo, Hatch 76, Simply Cheesecake and The Brownie Bar are just some of the popular North East food vendors at this weekend's Feast by the Sea.",
-    url: 'https://www.chroniclelive.co.uk/whats-on/food-drink-news/first-proper-food-drink-festival-12954941'
+    excerpt: 'Founders Mark and Shelley Deakin have announced the full list of their festivals for the year with some tasty new additions',
+    url: 'https://www.chroniclelive.co.uk/whats-on/food-drink-news/first-proper-food-drink-festival-12954941',
+    img: '/images/press-4.webp'
   }
 ]
