f42fdd26df27d26357dde10ceabffea4b2333022
Craig Abbott <admin@craigabbott.co.uk>
2022-11-23 20:59:02 +0000

    Added details and fb groups

--- src/app/data/events.mjs
+++ src/app/data/events.mjs
@@ -13,6 +13,18 @@ const events = [
       city: 'South Shields',
       postcode: 'NE33 2LA'
     },
+    details: `
+      <p>
+        Bents park is a huge event space overlooking the sea and situation between 
+        Sheerans amusement park and marine park, with it's steam train, bars and 
+        restaurants.
+      </p>
+      <p>
+        We've ran these event for many years, they attract thousands of visitors 
+        each year and are a favourite on the local calendar.
+      </p>
+    `,
+    fbgroup: 'https://fb.me/e/36KtBX1rX',
     articles: ['1', '2'],
     map: 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2288.516753178786!2d-1.419454183928772!3d54.999106280358525!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x487e6ed9d424b259%3A0x7fbe717441e4332f!2sBents%20Park!5e0!3m2!1sen!2suk!4v1667737339904!5m2!1sen!2suk',
     prices: [
@@ -29,7 +41,7 @@ const events = [
       {
         id: '3',
         name: 'Makers market - Space only',
-        cost: 140
+        cost: 100
       },
       {
         id: '4',
@@ -49,7 +61,7 @@ const events = [
       src: '/images/event-whitley-bay-1.jpg',
       alt: ''
     },
-    name: 'Feast',
+    name: 'Feast by the sea',
     date: '17 June to 18 June 2023',
     address: {
       line1: 'Spanish City Plaza',
@@ -57,6 +69,21 @@ const events = [
       city: 'Whitley Bay',
       postcode: 'NE26 1BG'
     },
+    fbgroup: 'https://fb.me/e/3aGRG24Gm',
+    details: `
+      <p>
+        Spanish City is a recently developed sea-front event space on the old 
+        amusement park. It is recognisable by the gleaming Spanish City dome home 
+        to cafes and shops.
+      </p>
+      <p>
+        We have been running events here for more than 10 years and are attended 
+        by thousands of visitors.
+      </p>
+      <p>
+        The sea-front and Park View shopping areas are also a must when visiting.
+      </p>
+    `,
     articles: ['3'],
     map: 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d571.4375095274908!2d-1.4486599034089518!3d55.04759554081662!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x487e6c2f6840d237%3A0xea596f7d81c24698!2sSpanish%20City!5e0!3m2!1sen!2suk!4v1667738077197!5m2!1sen!2suk',
     prices: [
@@ -78,7 +105,7 @@ const events = [
       src: '/images/event-cramlington-1.jpg',
       alt: ''
     },
-    name: 'Feast',
+    name: 'Cramlington Feast',
     date: '1 July to 2 July 2023',
     address: {
       line1: 'Seven Oaks Park',
@@ -86,6 +113,21 @@ const events = [
       city: 'Cramlington',
       postcode: 'NE23 6US'
     },
+    details: `
+      <p>
+        Seven Oaks Park is a 2 minute stroll through the underpass from Manor Walks, 
+        and bang in the centre of Cramlington Village.
+      </p>
+      <p>
+        It is a fantastic venue with a village feel, whilst attracting big 
+        numbers.
+      </p>
+      <p>
+        Our 2023 addition, as well as all the usual food and drink festivals, will 
+        feature a holistic wellbeing area.
+      </p>
+    `,
+    fbgroup: 'https://fb.me/e/5f69z8vAE',
     articles: ['4'],
     map: 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1475.616458909384!2d-1.5852965415333604!3d55.085060391413!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x487e73087499626d%3A0x51ba3e47c361f0ae!2sSeven%20Oaks%20Park!5e0!3m2!1sen!2suk!4v1667738155967!5m2!1sen!2suk"',
     prices: [
@@ -102,7 +144,7 @@ const events = [
       {
         id: '3',
         name: 'Makers market - Space only',
-        cost: 180
+        cost: 100
       },
       {
         id: '4',
@@ -122,14 +164,29 @@ const events = [
       src: '/images/event-whitley-bay-2.jpg',
       alt: ''
     },
-    name: 'Feast',
-    date: '8 July to 9 July 2023',
+    name: 'Feast by the sea',
+    date: '22 July to 23 July 2023',
     address: {
       line1: 'Spanish City Plaza',
       line2: '',
       city: 'Whitley Bay',
       postcode: 'NE26 1BG'
     },
+    details: `
+      <p>
+        Spanish City is a recently developed sea-front event space on the old 
+        amusement park. It is recognisable by the gleaming Spanish City dome home 
+        to cafes and shops.
+      </p>
+      <p>
+        We have been running events here for more than 10 years and are attended 
+        by thousands of visitors.
+      </p>
+      <p>
+        The sea-front and Park View shopping areas are also a must when visiting.
+      </p>
+    `,
+    fbgroup: 'https://fb.me/e/3E2Ze9Yq7',
     articles: ['3'],
     map: 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d571.4375095274908!2d-1.4486599034089518!3d55.04759554081662!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x487e6c2f6840d237%3A0xea596f7d81c24698!2sSpanish%20City!5e0!3m2!1sen!2suk!4v1667738077197!5m2!1sen!2suk',
     prices: [
@@ -151,14 +208,29 @@ const events = [
       src: '/images/event-whitley-bay-3.jpg',
       alt: ''
     },
-    name: 'Feast',
-    date: '19 August to 20 August 2023',
+    name: 'Feast by the sea',
+    date: '12 August to 13 August 2023',
     address: {
       line1: 'Spanish City Plaza',
       line2: '',
       city: 'Whitley Bay',
       postcode: 'NE26 1BG'
     },
+    details: `
+      <p>
+        Spanish City is a recently developed sea-front event space on the old 
+        amusement park. It is recognisable by the gleaming Spanish City dome home 
+        to cafes and shops.
+      </p>
+      <p>
+        We have been running events here for more than 10 years and are attended 
+        by thousands of visitors.
+      </p>
+      <p>
+        The sea-front and Park View shopping areas are also a must when visiting.
+      </p>
+    `,
+    fbgroup: 'https://fb.me/e/5CrsO6hR4',
     articles: ['3'],
     map: 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d571.4375095274908!2d-1.4486599034089518!3d55.04759554081662!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x487e6c2f6840d237%3A0xea596f7d81c24698!2sSpanish%20City!5e0!3m2!1sen!2suk!4v1667738077197!5m2!1sen!2suk',
     prices: [
@@ -188,6 +260,18 @@ const events = [
       city: 'South Shields',
       postcode: 'NE33 2LA'
     },
+    details: `
+      <p>
+        Bents park is a huge event space overlooking the sea and situation between 
+        Sheerans amusement park and marine park, with it's steam train, bars and 
+        restaurants.
+      </p>
+      <p>
+        We've ran these event for many years, they attract thousands of visitors 
+        each year and are a favourite on the local calendar.
+      </p>
+    `,
+    fbgroup: 'https://fb.me/e/6gMlViiec',
     articles: ['1', '2'],
     map: 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2288.516753178786!2d-1.419454183928772!3d54.999106280358525!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x487e6ed9d424b259%3A0x7fbe717441e4332f!2sBents%20Park!5e0!3m2!1sen!2suk!4v1667737339904!5m2!1sen!2suk',
     prices: [
@@ -204,7 +288,7 @@ const events = [
       {
         id: '3',
         name: 'Makers market - Space only',
-        cost: 140
+        cost: 100
       },
       {
         id: '4',
