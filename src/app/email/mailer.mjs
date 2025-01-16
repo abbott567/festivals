100ea2c4bde77fb165952b256fbaeb2376dd447d
Craig Abbott <admin@craigabbott.co.uk>
2025-01-16 21:47:44 +0000

    Modularised emails

--- src/app/email/mailer.mjs
+++ src/app/email/mailer.mjs
@@ -1,41 +1,60 @@
 'use-strict'
 
 import nodemailer from 'nodemailer'
+import nunjucks from 'nunjucks'
 
-function sendEmail (recipients, text, html) {
-// create reusable transporter object using the default SMTP transport
-  const transporter = nodemailer.createTransport({
-    host: process.env.MAIL_HOST,
-    port: process.env.MAIL_PORT,
-    secure: process.env.MAIL_SECURE === 'true',
-    auth: {
-      user: process.env.MAIL_AUTH_USER,
-      pass: process.env.MAIL_AUTH_PASSWORD
-    },
-    ignoreTLS: true
-  })
-
-  // setup email data with unicode symbols
-  const mailOptions = {
-    from: `"${process.env.EMAIL_FROM}" <${process.env.FESTIVAL_EMAIL}>`,
-    to: recipients,
-    subject: `${process.env.EMAIL_SUBJECT}`,
-    text,
-    html
-  }
-  // send mail with defined transport object
-  const tryToMail = new Promise((resolve, reject) => {
-    transporter.sendMail(mailOptions, (error, info) => {
-      if (error) {
-        console.log(error)
-        resolve(false)
-      } else {
-        console.log('Message %s sent: %s', info.messageId, info.response)
-        resolve(true)
-      }
+async function attemptEmail (recipients, text, html) {
+  try {
+    // create reusable transporter object using the default SMTP transport
+    const transporter = nodemailer.createTransport({
+      host: process.env.MAIL_HOST,
+      port: process.env.MAIL_PORT,
+      secure: process.env.MAIL_SECURE === 'true',
+      auth: {
+        user: process.env.MAIL_AUTH_USER,
+        pass: process.env.MAIL_AUTH_PASSWORD
+      },
+      ignoreTLS: true
     })
-  })
-  return tryToMail
+
+    // setup email data with unicode symbols
+    const mailOptions = {
+      from: `"${process.env.EMAIL_FROM}" <${process.env.FESTIVAL_EMAIL}>`,
+      to: recipients,
+      subject: `${process.env.EMAIL_SUBJECT}`,
+      text,
+      html
+    }
+    // send mail with defined transport object
+    const info = await transporter.sendMail(mailOptions)
+    console.log(`Email sent successfully! Message ID: ${info.messageId}`)
+    return true
+  } catch (error) {
+    console.error(`Email sending failed: ${error.message}`, error.stack)
+    return false
+  }
+}
+
+async function sendEmail (req) {
+  const data = req.session.data
+  const text = nunjucks.render('emails/template-text.njk', { data })
+  const html = nunjucks.render('emails/template-html.njk', { data })
+  const customerEmail = data.owner.email
+  const festivalEmail = process.env.FESTIVAL_EMAIL
+  const recipients = `${customerEmail}, ${festivalEmail}`
+
+  const emailed = await attemptEmail(recipients, text, html)
+  if (emailed) {
+    req.session.success = true
+    return 'success'
+  } else {
+    const errors = [{
+      id: 'email-failed',
+      message: 'Your email didn\'t send. Try again or call us on 0771111111',
+      hint: 'Try again or call us on 0741111111'
+    }]
+    return errors
+  }
 }
 
 export default sendEmail
--- src/app/routes/attend-as-a-vendor/check-your-answers.mjs
+++ src/app/routes/attend-as-a-vendor/check-your-answers.mjs
@@ -1,7 +1,6 @@
 'use-strict'
 
 import sendEmail from '../../email/mailer.mjs'
-import nunjucks from 'nunjucks'
 
 function addToRoutes (router) {
   // Check your answers
@@ -26,26 +25,10 @@ function addToRoutes (router) {
     }
   })
   router.post('/attend-as-a-vendor/check-your-answers', async (req, res) => {
-    const data = req.session.data
-    const text = nunjucks.render('emails/template-text.njk', { data })
-    const html = nunjucks.render('emails/template-html.njk', { data })
-    const customerEmail = data.owner.email
-    const festivalEmail = process.env.FESTIVAL_EMAIL
-    const recipients = `${customerEmail}, ${festivalEmail}`
-    const emailed = await sendEmail(recipients, text, html)
-    if (emailed) {
-      req.session.success = true
-      return res.redirect('/attend-as-a-vendor/success')
-    } else {
-      const errors = [{
-        id: 'email-failed',
-        message: 'Your email didn\'t send. Try again or call us on 0771111111',
-        hint: 'Try again or call us on 0741111111'
-      }]
-      return res.render('pages/attend-as-a-vendor/check-your-answers/template.njk', { errors })
-    }
+    const emailResult = await sendEmail(req)
+    if (emailResult === 'success') res.redirect('/attend-as-a-vendor/success')
+    else res.render('pages/attend-as-a-vendor/check-your-answers/template.njk', { errors: emailResult })
   })
-  // const templateHTML = '../../../src/views/emails/template-html.njk'
 }
 
 export default addToRoutes
